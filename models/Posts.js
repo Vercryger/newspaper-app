8c9d7c4b44da49fd01e0febeeb81312438de8f38
Vercryger <abuzaid.karim@gmail.com>
2015-05-07 17:56:44 -0300

    RELEASE v1.0

---
+++ models/Posts.js
@@ -0,0 +1,21 @@
+var mongoose = require('mongoose');
+
+var PostSchema = new mongoose.Schema({
+  title: String,
+  link: String,
+  upvotes: {type: Number, default: 0},
+  author: String,
+  comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }]
+});
+
+PostSchema.methods.upvote = function(cb) {
+  this.upvotes += 1;
+  this.save(cb);
+};
+
+PostSchema.methods.downvote = function(cb) {
+  this.upvotes -= 1;
+  this.save(cb);
+};
+
+mongoose.model('Post', PostSchema);
